80706cc840e6ead7066c6fa8659b4ba40bd5e2c2
Yusuke-Miyajima <yuten7.09.01@gmail.com>
2019-10-20 02:13:47 +0000

    updated body,lens show

--- app/models/body.rb
+++ app/models/body.rb
@@ -1,3 +1,6 @@
 class Body < ApplicationRecord
 	has_many :photos, dependent: :destroy
+	enum film: { film: 1, degital:2 }
+	enum reflex: { reflex: 1, mirrorless: 2 }
+	enum interchangeable: { interchangeable: 1, built_in: 2 }
 end
--- app/models/lens.rb
+++ app/models/lens.rb
@@ -1,4 +1,5 @@
 class Lens < ApplicationRecord
 	has_many :photos, dependent: :destroy
 	self.inheritance_column = :_type_disabled
+	enum full_frame_support: { able: 1, aps_c: 2 }
 end
